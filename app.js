c17a22596c7da2b689326dae5d42cc67085079e1
steve2482 <stevecb12684@yahoo.com>
2016-07-28 17:09:45 -0400

    refracted

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 $(document).ready(function() {
-	for (var counter = 0; counter < 101; counter++) {
-		var value = counter
-		if (value % 15 === 0) {
-			value = "fizzbuzz";
-		}	
-		if (value % 3 === 0) {
-			value = "fizz";
-		}
-		if (value % 5 === 0) {
-			value = "buzz";
-		}
-		$(".values").append(
-			'<li class="val_list">' +
-			value +
-			'</li>'
-			);
-		
-	}	
+'use strict';
+// User Prompt-------------------------------------------------------
+var userInput = parseInt(prompt("Please enter a number.", "82"));
+  while ( isNaN(userInput) ) {
+    alert("Please enter a numerical value!");
+    userInput = parseInt(prompt("Please enter a number."));
+  }
+ // FizzBuzz counter-------------------------------------------------
+for ( var counter = 1; counter <= userInput; counter++) {
+  var value = counter
+  if (value % 15 === 0) {
+  	value = "fizzbuzz";
+  }
+  if (value % 3 === 0) {
+  	value = "fizz";
+  }
+  if (value % 5 === 0) {
+  	value = "buzz";
+  }
+  $(".values").append(
+  	'<li class="val_list">' + 
+  	value + 
+  	'</li>'
+  	);
+  }
 });
